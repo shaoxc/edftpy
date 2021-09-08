7483a24951baa234567ecf427ceb51bebec2141f
shaoxc <shaoxcnmg@gmail.com>
2021-09-08 11:55:22 -0400

    update the version

--- edftpy/__init__.py
+++ edftpy/__init__.py
@@ -2,4 +2,11 @@ __author__ = "Pavanello Research Group"
 __contact__ = "m.pavanello@rutgers.edu"
 __license__ = "MIT"
 __version__ = "0.0.1"
-__date__ = "2020-05-08"
+__date__ = "2021-09-08"
+
+from importlib.metadata import version, PackageNotFoundError
+
+try:
+    __version__ = version("edftpy")
+except PackageNotFoundError:
+    pass
