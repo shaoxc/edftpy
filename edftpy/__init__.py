e417d7ed59661499b2097e54fe9eec7120a5bc94
shaoxc <shaoxcnmg@gmail.com>
2021-10-04 22:24:08 -0400

    importlib-metadata for python<3.8

--- edftpy/__init__.py
+++ edftpy/__init__.py
@@ -4,9 +4,15 @@ __license__ = "MIT"
 __version__ = "0.0.1"
 __date__ = "2021-09-08"
 
-from importlib.metadata import version, PackageNotFoundError
+try:
+    from importlib.metadata import version # python >= 3.8
+except Exception :
+    try:
+        from importlib_metadata import version
+    except Exception :
+        pass
 
 try:
     __version__ = version("edftpy")
-except PackageNotFoundError:
+except Exception:
     pass
